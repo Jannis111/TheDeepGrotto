910cc580213ac03ae9fdce94862328800d389471
Jochem Jansen <jd.jansen2@student.han.nl>
2023-03-25 13:57:43 +0100

    Nog wat nieuwe files

--- src/main/java/com/github/hanyaeger/tutorial/entities/Henk.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/Henk.java
@@ -4,7 +4,6 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
-import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import com.github.hanyaeger.tutorial.entities.map.Wall;
